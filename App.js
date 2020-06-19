a902e91ae3054644d935e1be67ba05cdb8de2e26
Edgar Olivar <edgarolivar16@gmail.com>
2020-06-18 20:40:35 -0400

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+const Texto = ({ content, children }) => (
+  <Text>{content ? content : children}</Text>
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <h1>words from h1 tag</h1>
+      <Texto content={"NOT the children"}>The children</Texto>
+      <Texto content={"Hello World"}></Texto>
+      <Texto content={"Bye World"}></Texto>
     </View>
   );
 }
@@ -12,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
