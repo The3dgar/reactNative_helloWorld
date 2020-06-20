c754b5f7df9ba7dfb1293233efffa003e0cdc192
Edgar Olivar <edgarolivar16@gmail.com>
2020-06-20 19:47:24 -0400

    test with Button

--- App.js
+++ App.js
@@ -1,37 +1,73 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-const Texto = ({ txt = "DefaultText", style }) => {
-  const [content, setContent] = useState(txt);
-  const updateText = () => {
-    let reverse = content.split("").reverse().join("")
-    setContent(reverse);
-  };
-  
-  return (
-    <Text style={[styles.customText, style]} onPress={updateText}>
-      {content}
-    </Text>
-  );
-};
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  Button,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+} from "react-native";
+// TouchableHighlight: only works in andrid
+// TouchableNativeFeedback: only works in android
+
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
+  const [text, setText] = useState("Initial value");
+  const [submit, setSubmit] = useState("");
+
+  const myActionOne = () => {
+    setSubmit(text);
+    alert("Texto enviado con exito");
+  };
+
   return (
     <View style={styles.container}>
-      <Texto style={styles.yellow}></Texto>
+      <Text>Texto: {submit}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Texto por defecto"
+        onChangeText={(t) => setText(t)}
+        defaultValue={text}
+      ></TextInput>
+      {/* <Button onPress={myActionOne} title="Aceptar" /> */}
+      <TouchableOpacity style={styles.TouchableOpacity} onPress={myActionOne}>
+        <View style={styles.view}>
+          <Text>AceptarTc</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  TouchableOpacity:{
+    backgroundColor:"#eee"
+  },
+  view: {
+    height: 40,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center"
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   yellow: {
     backgroundColor: "yellow",
     color: "red",
-    fontSize: 24
-  }
+    fontSize: 24,
+  },
+  input: {
+    height: 40,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    width: "100%",
+  },
 });
