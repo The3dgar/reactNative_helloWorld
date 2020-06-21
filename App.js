649b250af10964c6c7f1b45ae05643b601bb6776
Edgar Olivar <edgarolivar16@gmail.com>
2020-06-20 21:30:39 -0400

    a little test with Images

--- App.js
+++ App.js
@@ -1,60 +1,34 @@
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
+  ActivityIndicator,
   Text,
   View,
   Platform,
   StatusBar,
-  FlatList,
+  Image,
+  ImageBackground,
 } from "react-native";
-import { render } from "react-dom";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // un efecto es una data que no conocemos su magnitud
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <Text>loading...</Text>
-      </View>
-    );
-  }
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        keyExtractor={(item) => String(item.id)}
-      />
+      <ActivityIndicator size="large" color="#00f"></ActivityIndicator>
+      <ImageBackground
+        style={styles.photo}
+        // source={require("./assets/icon.png")}
+        source={{ uri: "https://placekitten.com/200/200" }}
+      >
+        <Text>Hola mundo</Text>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-    width: "100%",
+  photo: {
+    height: 200,
+    width: 200,
   },
   container: {
     flex: 1,
