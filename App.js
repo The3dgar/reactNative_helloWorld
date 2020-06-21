bc0f8a466cf498711431ca3c7cf410b04c7745fb
Edgar Olivar <edgarolivar16@gmail.com>
2020-06-20 22:16:07 -0400

    test of native alert

--- App.js
+++ App.js
@@ -7,44 +7,27 @@ import {
   StatusBar,
   Modal,
   Button,
+  Alert,
 } from "react-native";
-
+const crearDialogo = () => {
+  Alert.alert(
+    "Titulo del alert",
+    "subtitulo del alert o mensaje contenido en el alert",
+    [
+      { text: "Cancelar", onPress: () => {}, style: "cancel" },
+      {
+        text: "Aceptar",
+        onPress: () => console.log("button press"),
+      },
+    ],
+    {cancelable: false}
+  );
+};
 export default function App() {
   const [modal, setModal] = useState(false);
   return (
     <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modal}
-        onRequestClose={() => {
-          // some code
-        }}
-      >
-        <View style={styles.center}>
-          <View style={styles.content}>
-            <Text>Soy un modal</Text>
-            <Button
-              title="Cerrar modal"
-              onPress={() => setModal(!modal)}
-            ></Button>
-          </View>
-        </View>
-      </Modal>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Text>No soy del Modal</Text>
-      <Button title="Abrir Modal" onPress={() => setModal(!modal)}></Button>
+      <Button title="Abrir alert" onPress={crearDialogo}></Button>
     </View>
   );
 }
@@ -52,17 +35,17 @@ export default function App() {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    backgroundColor:"#fff",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     margin: 25,
-    borderRadius: 5
+    borderRadius: 5,
   },
   center: {
     flex: 1,
     alignItems: "stretch",
     justifyContent: "center",
-    backgroundColor: "rgba(0,0,0,0.3)"
+    backgroundColor: "rgba(0,0,0,0.3)",
   },
   container: {
     flex: 1,
