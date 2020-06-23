be0187864035005878302330af9ee6d3ac8b7970
Edgar Olivar <edgarolivar16@gmail.com>
2020-06-22 23:02:37 -0400

    Maps with Mapview && react-native-maps

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [locacion, setLocacion] = useState({});
+
   const buscaLocation = async () => {
     const { status } = await Location.requestPermissionsAsync();
-    if(status !== "granted") {
-      return alert("No tenemos permisos")
+    if (status !== "granted") {
+      return alert("No tenemos permisos");
     }
 
-    const location = await Location.getCurrentPositionAsync({})
-    console.log(location);
+    const location = await Location.getCurrentPositionAsync({});
+    setLocacion(location);
   };
   useEffect(() => {
     buscaLocation();
   });
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <MapView style={styles.map}>
+        {locacion.coords ? (
+          <Marker
+            coordinate={locacion.coords}
+            title="Casita"
+            description="Casita de julianny"
+          ></Marker>
+        ) : null}
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
   content: {
     flex: 1,
     backgroundColor: "#fff",
